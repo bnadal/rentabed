191a71dbd88470b2e054faf939dfb9c098d086ee
Daniyar Bazakov <bazakovd@gmail.com>
2019-11-19 15:33:32 -0500

    added view_my_booking

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'ads#home'
-  get 'ads/myads', to: 'users#myads', as: :myads
+  #get 'bookings/mybookings', to: 'users#mybookings', as: :mybookings
+  #get 'ads/myads', to: 'users#myads', as: :myads
   resources :ads, only: [:index, :show, :create, :new, :destroy]
-  
+  resources :users, only: [:index, :show, :create, :new, :destroy]
+  resources :bookings, only: [:index, :show, :create, :new, :destroy]
 end
