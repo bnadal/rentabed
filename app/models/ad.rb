17fcb77fb1d3012ee4af8d964810d7661975ac72
Daniyar Bazakov <bazakovd@gmail.com>
2019-11-22 14:27:05 -0500

    search bar done

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -2,6 +2,13 @@ class Ad < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
+include PgSearch::Model
+  pg_search_scope :search_by_title,
+    against: [ :title ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
+
   def pending?(user, ad_id)
     booking = self.bookings.where(user: user, ad: ad_id).first
     if booking.present?
