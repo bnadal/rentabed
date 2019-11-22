99549852150fce680afdf06c01ca2ada283442ad
Philippe Charpentier <jovialiste82@yahoo.fr>
2019-11-22 11:28:08 -0500

    fixed ad status and login alert

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :ad
+  after_save :update_ad_status
+  after_destroy :make_ad_available
 
   def pending?(user, ad_id)
     self.bookings.where(user: user, ad: ad_id).first.status == "pending"
@@ -8,5 +10,19 @@ class Booking < ApplicationRecord
 
   def declined?(user, ad_id)
     self.bookings.where(user: user, ad: ad_id).first.status == "declined"
+  end
+
+  def update_ad_status
+    if self.status == "confirmed" || self.status == "pending"
+      self.ad.update(status: "unavailable")
+    else
+      self.ad.update(status: "available")
+    end
+  end
+
+  def make_ad_available
+    if self.ad != nil
+      self.ad.update(status: "available")
+    end
   end
 end
